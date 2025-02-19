c0ae766745ad83d3b358b4034a5fa02a2a6fb5af
Vasyl Potiuk <potiuk.vasyl@student.uzhnu.edu.ua>
2025-02-19 14:05:14 +0200

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('bac', function(req, res, next) {
+  res.send('respond with a resource 2');
+});
+
+
+module.exports = router;
